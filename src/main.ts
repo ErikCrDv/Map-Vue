a6fb26b883b3f15975c5b72509f12dfa7d65faa6
Erik Cordova <ErikCrDv@outlook.com>
2022-01-20 22:57:26 -0600

    Implementacion de distancia y tiempo

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+mapboxgl.accessToken = 'pk.eyJ1IjoiZXJpa2NyZHYiLCJhIjoiY2t5YzV4NWZhMDd4aTJxbW80aTkyM3RkZCJ9.XQxYn91irw41eSx7IbGLMA';
+
+if( !navigator.geolocation ) 
+    throw new Error('Tu navegador no soporta el GeoLocation')
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app')
